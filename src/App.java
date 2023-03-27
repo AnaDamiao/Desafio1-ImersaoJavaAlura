ff66cdc0c0ef02ddc937a7cc93b923c5bf804b90
AnaDamiao <damiaoclara2003@gmail.com>
2023-03-27 20:56:32 -0300

    decorando terminal

--- src/App.java
+++ src/App.java
@@ -11,7 +11,7 @@ public class App {
 
         
         // fazer uma conexão HTTP e buscar os top 10 filmes
-        String urlApi = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        String urlApi = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
         URI endereco = URI.create(urlApi);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -24,14 +24,24 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
         
         // exibir e manipular os dados
+        System.out.println("⭐ TOP 10 MOVIES ⭐");
+        for (int i = 0; i < 3; i++) {
+            Map<String,String> filme = listaDeFilmes.get(i); 
+            System.out.println("\u001b[1m\u001b[3mFilme: \u001b[m" + "\u001b[38;2;255;255;255m \u001b[48;2;255;126;249m" + filme.get("title") + "\u001b[0m");
+            System.out.println("\u001b[1m\u001b[3mImagem: \u001b[m" + filme.get("image"));
+            double classific = Double.parseDouble(filme.get("imDbRating"));
+            int numeroEstrelinhas = (int) classific;
+            if (classific <= 5) {
+                System.out.println("\u001b[38;2;255;255;255m \u001b[48;2;255;0;0mclsClassificação: (" + classific + ")\u001b[0m");
+            } else {
+                System.out.println("\u001b[38;2;255;255;255m \u001b[48;2;42;255;0mClassificação: (" + classific + ")\u001b[0m");
+            }
+            
+            for (int estrelinhas = 1; estrelinhas <= numeroEstrelinhas; estrelinhas++) {
+                System.out.print("⭐");
+            }
+            System.out.println("\n");
 
-        for (Map<String,String> filme : listaDeFilmes) {
-            //System.out.println("\u001b[33m\u001b[1m Título: \u001b[0m " + filme.get("title"));
-            System.out.println("Título" + filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("rank"));
-            System.out.println();
-        }
-
-    }
+         }
+     }
 }
